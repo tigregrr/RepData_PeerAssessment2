cc34bf7f9263b1dae048f2fc4d240d1afac3b0e4
luis_urbina <urbinazarate@aol.com>
2015-05-22 15:11:12 -0500

    t440

--- proj.R
+++ proj.R
@@ -14,36 +14,54 @@ data = read.csv(file=url) #, ssl.verifypeer=FALSE
 
 data = read.csv(bzfile('repdata-data-StormData.csv.bz2'))
 dim(data)
-
+#[1] 902297     37
 str(data)
 summary(data)
 
-# harmful for population health 
-max(data$FATALITIES)
-data[data$FATALITIES==583,]
 
-max(data$INJURIES)
-data[data$INJURIES=1700,]
+da <- data[,c(2,7,8,22,23,24,25,26,27,28)]
 
-hist(data$FATALITIES)
-hist(data$INJURIES)
+> max(da$PROPDMG)
+[1] 5000
+> unique(da$PROPDMGEXP)
+Levels:  - ? + 0 1 2 3 4 5 6 7 8 B h H K m M
 
-> mean(data$INJURIES)
-[1] 0.1557447
-> median(data$INJURIES)
-[1] 0
-> mean(data$FATALITIES)
-[1] 0.01678494
-> median(data$FATALITIES)
-[1] 0
+> max(da$CROPDMG)
+[1] 990
+unique(da$CROPDMGEXP)
+Levels:  ? 0 2 B k K m M
 
-count for NA elements
+da.b <- da[(da$PROPDMGEXP=='B')|da$CROPDMGEXP=='B',]
+View(da.b)
+
+# identify
+I prefer following way to check whether rows contain any NAs:
+    
+    row.has.na <- apply(final, 1, function(x){any(is.na(x))})
+This returns logical vector with values denoting whether there is any NA in a row. You can use it to see how many rows you'll have to drop:
+
+sum(row.has.na)
+and eventually drop them
+
+final.filtered <- final[!row.has.na,]
+For filtering rows with certain part of NAs it becomes a little trickier (for example, you can feed 'final[,5:6]' to 'apply'). Generally, Joris Meys' solution seems to be more elegant.
 
 
-# for greatest economic consequences
-max(data$PROPDMG)
-max(data$CROPDMG)
-##############
+# replacement
+interval.mean
+for (i in 1:(length(da.full[,1]) )) {
+    if (is.na(da.full$steps[i])) 
+        da.full$steps[i] <- interval.mean
+}
+
+# identifying blank values
+
+tidy.NA <- data[,c(22,23)]
+na.identifier <- apply(tidy.NA, 1, function(x){ifelse(x=="", TRUE, FALSE)})
+total.NA <- sum(na.identifier)
+total.NA
 
 
-```
+tidy.NA <- data[,c(26,28)]
+na.identifier <- apply(tidy.NA, 1, function(x){ifelse(x=="", 1, x)})
+View(na.identifier)
